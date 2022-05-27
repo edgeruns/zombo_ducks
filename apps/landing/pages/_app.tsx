eb017ae508eb00ce858caa6c60dde3caa36fe7b3
Artur Kudashev <art-kudashev@mail.ru>
2022-05-27 20:05:46 +0500

    feat: add DOMAIN_HOST to env

--- apps/landing/pages/_app.tsx
+++ apps/landing/pages/_app.tsx
@@ -9,16 +9,6 @@ function App(props: AppProps) {
     return (
         <>
             <Head>
-                <meta property="og:url" content="https://zombo-ducks.xyz" />
-                <meta property="og:title" content="ZomboDucks" />
-                <meta property="og:description" content="Meet ZomboDucks: First P2E gameplace platform on NEAR protocol" />
-                <meta property="og:type" content="website" />
-                <meta property="og:image" content="https://zombo-ducks.xyz/og.png" />
-
-                <meta name="description" content="Meet ZomboDucks: First P2E gameplace platform on NEAR protocol" />
-
-                <link rel="canonical" href="https://zombo-ducks.xyz" />
-
                 <title>ZomboDucks</title>
             </Head>
 
