0a214d543293248f5644da68d3eb72b34988fe61
Artur Kudashev <art-kudashev@mail.ru>
2022-10-06 23:37:00 +0700

    fix: reset background items on mobile and tablet

--- libs/landing/components/src/Background/index.tsx
+++ libs/landing/components/src/Background/index.tsx
@@ -1,6 +1,6 @@
-import React, { FC, useRef, useEffect } from 'react'
+import React, { FC, useRef, useEffect, useCallback } from 'react'
 
-import { useBreakpoint,  } from '@apps/utils'
+import { useBreakpoint } from '@apps/utils'
 
 import styles from './Background.module.scss'
 
@@ -9,40 +9,58 @@ export const Background: FC = () => {
 
     const wrapperRef = useRef<HTMLDivElement>(null)
 
-    // @ts-ignore
-    useEffect(() => {
+    const moveItems = useCallback((event: MouseEvent) => {
         const wrapper = wrapperRef.current
         const items = wrapper?.children
         const next = document.querySelector<HTMLDivElement>('#__next')
 
-        if (items && next && breakpoint === 'desktop') {
-            const onMouseMove = (event: MouseEvent) => {
-                for (let i = 0; i < items.length; i++) {
-                    const clientX = event.clientX - next.offsetLeft
-                    const clientY = event.clientY + document.documentElement.scrollTop
+        if (items && next) {
+            for (let i = 0; i < items.length; i++) {
+                const clientX = event.clientX - next.offsetLeft
+                const clientY = event.clientY + document.documentElement.scrollTop
 
-                    const item = items[i] as HTMLElement
-                    const itemXOffset = window.innerWidth - next.offsetLeft - item.offsetLeft - item.offsetWidth / 2
-                    const itemYOffset = item.offsetTop - item.offsetHeight / 2
+                const item = items[i] as HTMLElement
+                const itemXOffset = window.innerWidth - next.offsetLeft - item.offsetLeft - item.offsetWidth / 2
+                const itemYOffset = item.offsetTop - item.offsetHeight / 2
 
-                    const speed = 4
+                const speed = 4
 
-                    const x = (clientX - itemXOffset) / speed
-                    const y = (clientY - itemYOffset) / speed
+                const x = (clientX - itemXOffset) / speed
+                const y = (clientY - itemYOffset) / speed
 
-                    const style = `transform: translate3d(${x}px, ${y}px, 0);`
+                const style = `transform: translate3d(${x}px, ${y}px, 0);`
 
-                    item.setAttribute('style', style)
-                }
-            }
-
-            window.addEventListener('mousemove', onMouseMove)
-
-            return () => {
-                window.removeEventListener('mousemove', onMouseMove)
+                item.setAttribute('style', style)
             }
         }
-    }, [breakpoint])
+    }, [])
+
+    const resetItems = useCallback(() => {
+        const wrapper = wrapperRef.current
+        const items = wrapper?.children
+
+        if (items) {
+            for (let i = 0; i < items.length; i++) {
+                const item = items[i]
+
+                item.setAttribute('style', '')
+            }
+        }
+    }, [])
+
+    useEffect(() => {
+        if (breakpoint === 'desktop') {
+            window.addEventListener('mousemove', moveItems)
+
+            return () => {
+                window.removeEventListener('mousemove', moveItems)
+            }
+        } else {
+            resetItems()
+        }
+
+        return undefined
+    }, [breakpoint, moveItems, resetItems])
 
     return (
         <div
