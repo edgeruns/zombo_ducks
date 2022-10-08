c93a1d9c5ec17f3d0d55dcce643e544003d0da7c
Anton Shchurov <ant.shchurov@gmail.com>
2022-10-09 02:29:25 +0700

    build: added deploy workflow

--- apps/games/zombofighter/api/src/app/app.module.ts
+++ apps/games/zombofighter/api/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { AppController } from './app.controller';
             port: +process.env.DB_PORT || 5432,
             username: process.env.DB_USER,
             password: process.env.DB_PASS,
+            ssl: true,
             host: process.env.DB_HOST,
             database: process.env.DB_NAME,
             entities: schemas,
