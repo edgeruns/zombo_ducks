233f43bd01e3e8bf16d1fd9f4b5d51663cc8ca76
Anton Shchurov <ant.shchurov@gmail.com>
2022-10-24 02:22:11 +0700

    feat: added business logic to smart contract

--- contracts/zombofighter/src/lib.rs
+++ contracts/zombofighter/src/lib.rs
@@ -1,12 +1,30 @@
 extern crate near_sdk;
 
-use std::collections::HashMap;
+use std::collections::{HashMap};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{near_bindgen, env, Promise, Balance, AccountId};
-use near_sdk::collections::UnorderedMap;
+use near_sdk::{near_bindgen, env, Promise, Balance, AccountId, BorshStorageKey, json_types::U128};
+use near_sdk::collections::{LookupMap, LookupSet};
+use near_sdk::serde::Serialize;
+
+#[derive(BorshSerialize, BorshStorageKey)]
+enum StorageKey {
+    Players,
+    Games,
+    Referees,
+}
+
 
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize)]
+#[derive(BorshSerialize, BorshDeserialize, Serialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct Statement {
+    owner: AccountId,
+    winner: Option<AccountId>,
+}
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize, Serialize)]
+#[serde(crate = "near_sdk::serde")]
 pub struct PlayerStatistic {
     pub wins: u32,
     pub losses: u32,
@@ -14,7 +32,8 @@ pub struct PlayerStatistic {
 }
 
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize)]
+#[derive(BorshDeserialize, BorshSerialize, Serialize)]
+#[serde(crate = "near_sdk::serde")]
 pub struct Player {
     pub id: AccountId,
     pub statistic: PlayerStatistic,
@@ -22,67 +41,79 @@ pub struct Player {
 }
 
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize)]
+#[derive(BorshDeserialize, BorshSerialize, Serialize)]
+#[serde(crate = "near_sdk::serde")]
 pub struct Game {
-    pub id: u32,
-    pub players: Vec<Player>,
+    pub id: String,
+    pub players: HashMap<AccountId, Player>,
+    pub statements: HashMap<AccountId, Statement>,
+    pub is_finished: bool,
     pub created_at: u64,
 }
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct ZomboFighter {
-    pub players: UnorderedMap<AccountId, Player>,
-    pub games: HashMap<u32, Game>,
-    pub referee: AccountId,
+    pub players: LookupMap<AccountId, Player>,
+    pub games: LookupMap<String, Game>,
+    pub referees: LookupSet<AccountId>,
+}
+
+impl Default for ZomboFighter {
+    fn default() -> Self {
+        Self {
+            players: LookupMap::new(StorageKey::Players),
+            games: LookupMap::new(StorageKey::Games),
+            referees: LookupSet::new(StorageKey::Referees),
+        }
+    }
 }
 
 #[near_bindgen]
 impl ZomboFighter {
-    #[init]
-    pub fn new(referee: AccountId) -> Self {
-        assert!(!env::state_exists(), "Already initialized");
-        Self {
-            players: UnorderedMap::new(b"p".to_vec()),
-            games: HashMap::new(),
-            referee,
-        }
-    }
-
     #[payable]
     pub fn deposit(&mut self) {
+        let account_id = env::predecessor_account_id();
         let deposit = env::attached_deposit();
-        let player_id = env::predecessor_account_id();
-        let mut player = self.players.get(&player_id).expect("Player not found");
+
+        let mut player = self.players.get(&account_id).expect("Player not found");
+
         player.deposit += deposit;
-        self.players.insert(&player_id, &player);
+
+        self.players.insert(&account_id, &player);
     }
 
     pub fn get_player(&self, id: AccountId) -> Player {
         self.players.get(&id).expect("Player not found")
     }
 
-    pub fn start_game(&mut self, player1_id: AccountId, player2_id: AccountId) -> u32 {
-        assert_eq!(env::predecessor_account_id(), self.referee, "Only referee can start a game");
+    pub fn start_game(&mut self, player1: AccountId, player2: AccountId, id: String) -> String {
+        assert!(self.referees.contains(&env::predecessor_account_id()), "Only referees can start games");
 
-        let game_id = self.games.len() as u32;
-        let player1 = self.players.get(&player1_id).expect("Player not found");
-        let player2 = self.players.get(&player2_id).expect("Player not found");
+        let mut players = HashMap::new();
+        let player1 = self.players.get(&player1).expect("Player not found");
+        let player2 = self.players.get(&player2).expect("Player not found");
+
+        players.insert(player1.id.clone(), player1);
+        players.insert(player2.id.clone(), player2);
 
         let game = Game {
-            id: game_id,
-            players: vec![
-                player1,
-                player2
-            ],
+            id,
+            players,
+            is_finished: false,
+            statements: HashMap::new(),
             created_at: env::block_timestamp(),
         };
-        self.games.insert(game_id, game);
-        game_id
+        self.games.insert(&game.id, &game);
+
+        game.id
     }
 
     pub fn register(&mut self) {
         let id = env::predecessor_account_id();
+
+        assert!(self.players.get(&id).is_none(), "Player already registered");
+
         let player = Player {
             id: id.clone(),
             statistic: PlayerStatistic {
@@ -96,27 +127,118 @@ impl ZomboFighter {
         self.players.insert(&id, &player);
     }
 
-    pub fn withdrawal(&mut self, amount: Balance) {
+    pub fn withdrawal(&mut self, amount: U128) {
         let player_id = env::predecessor_account_id();
         let mut player: Player = self.players.get(&player_id).expect("Player not found");
 
-        assert!(player.deposit >= amount, "Not enough deposit");
+        assert!(player.deposit >= amount.0, "Not enough deposit");
 
-        player.deposit -= amount;
+        player.deposit -= amount.0;
         self.players.insert(&player_id, &player);
-        Promise::new(player_id).transfer(amount);
+        Promise::new(env::predecessor_account_id()).transfer(amount.0);
     }
 
-    pub fn get_game(&self, id: u32) -> &Game {
+    pub fn get_game(&self, id: String) -> Game {
         self.games.get(&id).expect("Game not found")
     }
 
-    #[private]
-    pub fn set_referee(&mut self, referee: AccountId) {
-        self.referee = referee;
+    pub fn accept_result(&mut self, game_id: String, player_id: AccountId, winner: Option<AccountId>) {
+        if player_id.clone() != env::predecessor_account_id() {
+            assert!(self.referees.contains(&env::predecessor_account_id()), "Only referees can accept the result");
+        }
+
+        let mut game = self.games.get(&game_id).expect("Game not found");
+        assert!(game.statements.get(&player_id).is_none(), "Player already accepted the result");
+
+        let statement = Statement {
+            owner: player_id.clone(),
+            winner,
+        };
+
+        game.statements.insert(player_id, statement);
+
+        if game.statements.len() >= 2 {
+            self.finish_game(&game_id);
+        }
+
+        self.games.insert(&game_id, &game);
     }
 
 
+    fn finish_game(&mut self, game_id: &String) {
+        let mut game = self.games.get(&game_id).expect("Game not found");
+        let is_draw = ZomboFighter::is_draw(&game.statements);
+
+        if is_draw {
+            for player in game.players.values() {
+                let mut player = self.players.get(&player.id).expect("Player not found");
+                player.statistic.draws += 1;
+                self.players.insert(&player.id, &player);
+            }
+        } else {
+            let winner = self.get_winner(game_id).expect("Winner not found");
+            let winner_player = game.players.get_mut(&winner).expect("Player not found");
+
+            winner_player.statistic.wins += 1;
+            winner_player.deposit += 1000000000000000000000000 / 2;
+
+
+            for player in game.players.values() {
+                if player.id != winner {
+                    let mut player = self.players.get(&player.id).expect("Player not found");
+                    player.statistic.losses += 1;
+                    player.deposit -= 1000000000000000000000000;
+
+                    self.players.insert(&player.id, &player);
+                }
+            }
+        }
+
+        game.is_finished = true;
+
+        self.games.insert(&game_id, &game);
+    }
+
+    fn is_draw(statements: &HashMap<AccountId, Statement>) -> bool {
+        for (_, statement) in statements {
+            if statement.winner.is_some() {
+                return false;
+            }
+        }
+
+        return true
+    }
+
+    fn get_winner(&mut self, game_id: &String) -> Option<AccountId> {
+        let mut winner: HashMap<AccountId, u8> = HashMap::new();
+        let game = self.games.get(&game_id).expect("Game not found");
+
+        for (_, statement) in game.statements {
+            if statement.winner.is_some() {
+                let player = winner.entry(statement.winner.unwrap()).or_insert(0);
+                *player += 1;
+            }
+        }
+
+        for (player, approves) in winner {
+            if approves >= 2 {
+                return Some(player);
+            }
+        }
+
+        return None
+    }
+
+
+    #[private]
+    pub fn add_referee(&mut self, id: AccountId) {
+        self.referees.insert(&id);
+    }
+
+    #[private]
+    pub fn remove_referee(&mut self, id: AccountId) {
+        self.referees.remove(&id);
+    }
 }
 
 #[cfg(all(test, not(target_arch = "wasm32")))]
@@ -141,7 +263,7 @@ mod tests {
     fn register() {
         let context = get_context(accounts(1));
         testing_env!(context);
-        let mut contract = ZomboFighter::new(accounts(0));
+        let mut contract = ZomboFighter::default();
         contract.register();
         let player = contract.get_player(accounts(1));
         assert_eq!(player.id, accounts(1));
@@ -151,7 +273,7 @@ mod tests {
     fn get_player() {
         let context = get_context(accounts(1));
         testing_env!(context);
-        let mut contract = ZomboFighter::new(accounts(3));
+        let mut contract = ZomboFighter::default();
         contract.register();
         let player = contract.get_player(accounts(1));
         assert_eq!(player.id, accounts(1));
@@ -161,7 +283,7 @@ mod tests {
     fn deposit() {
         let context = get_context(accounts(1));
         testing_env!(context);
-        let mut contract = ZomboFighter::new(accounts(3));
+        let mut contract = ZomboFighter::default();
         contract.register();
         contract.deposit();
         let player = contract.get_player(accounts(1));
@@ -172,22 +294,51 @@ mod tests {
     fn withdrawal() {
         let context = get_context(accounts(1));
         testing_env!(context);
-        let mut contract = ZomboFighter::new(accounts(3));
+        let mut contract = ZomboFighter::default();
         contract.register();
         contract.deposit();
-        contract.withdrawal(1000000000000000000000000);
+        contract.withdrawal(U128::from(1000000000000000000000000));
         let player = contract.get_player(accounts(1));
         assert_eq!(player.deposit, 0);
     }
 
     #[test]
     fn start_game() {
+        let mut contract = ZomboFighter::default();
+
         let player_1_context = get_context(accounts(1));
         let player_2_context = get_context(accounts(2));
         let referee_context = get_context(accounts(3));
+        contract.add_referee(accounts(3));
+
         testing_env!(player_1_context);
 
-        let mut contract = ZomboFighter::new(accounts(3));
+        contract.register();
+        contract.deposit();
+
+
+        testing_env!(player_2_context);
+
+        contract.register();
+        contract.deposit();
+
+        testing_env!(referee_context);
+
+        contract.start_game(accounts(1), accounts(2), "game-1".to_string());
+        let game = contract.get_game("game-1".to_string());
+        assert_eq!(game.players.len(), 2);
+    }
+
+    #[test]
+    fn accept_result() {
+        let mut contract = ZomboFighter::default();
+
+        let player_1_context = get_context(accounts(1));
+        let player_2_context = get_context(accounts(2));
+        let referee_context = get_context(accounts(3));
+        contract.add_referee(accounts(3));
+
+        testing_env!(player_1_context);
 
         contract.register();
         contract.deposit();
@@ -199,8 +350,15 @@ mod tests {
 
         testing_env!(referee_context);
 
-        contract.start_game(accounts(1), accounts(2));
-        let game = contract.get_game(0);
-        assert_eq!(game.players.len(), 2);
+        contract.start_game(accounts(1), accounts(2), "game-1".to_string());
+
+
+        contract.accept_result("game-1".to_string(), accounts(1), Some(accounts(1)));
+        contract.accept_result("game-1".to_string(), accounts(2),  Some(accounts(2)));
+        contract.accept_result("game-1".to_string(), accounts(3),  Some(accounts(2)));
+
+        assert!(contract.get_game("game-1".to_string()).is_finished);
+        assert_eq!(contract.get_player(accounts(1)).statistic.losses, 1);
+        assert_eq!(contract.get_player(accounts(2)).statistic.wins, 1);
     }
 }
